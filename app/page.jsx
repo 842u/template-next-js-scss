8533d7cf32a934bfcd84c991a1ef043d38685667
Kamil Bażanow <kamil.bazanow@gmail.com>
2024-06-20 11:52:35 +0200

    chore: initial project setup

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+import styles from './page.module.scss';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>🫡</h1>
+    </main>
+  );
+}
